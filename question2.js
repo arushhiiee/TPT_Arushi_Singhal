3165385912a717a10bca67d823a53dae006cb664
Arushi <arushi.singhal.511@gmail.com>
2024-07-24 22:09:07 +0530

    Question 2 done

---
+++ question2.js
@@ -0,0 +1,21 @@
+//using if-else 
+let x = 50;
+let y = 1000;
+let max;
+
+if (x > y) {
+  max = x;
+} else {
+  max = y;
+}
+
+console.log("The larger value is: " + max); // Output: The larger value is: 1000
+
+//using ternary operators 
+let x1 = 6;
+let y1 = 10;
+
+let max1 = (x1 > y1) ? x1 : y1;
+
+console.log("The larger value is: " + max1); // Output: The larger value is: 10
+
